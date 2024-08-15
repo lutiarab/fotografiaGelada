4dbba2c74395d1262aa0c4742ce0a723f291b15f
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-08-15 10:51:44 -0300

    nova alteração

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const nodemon = require('nodemon');
+const cors = require('cors');
+
+const dotenv = require('dotenv');
+dotenv.config();
